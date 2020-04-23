0487129b161f7519443e8449b93b1f63d42ff0f4
Hariraagavanthar C R <hari.raghavandhar@myntra.com>
2020-04-23 12:08:45 +0530

    Updates

---
+++ precache-manifest.ef1329bed270df207e214bfb2cb300b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f645d31fb92ff4726befe92efb5fbb3",
+    "url": "/laxx/index.html"
+  },
+  {
+    "revision": "72f12c6b13f14bd5f44c",
+    "url": "/laxx/static/css/main.03e79eb6.chunk.css"
+  },
+  {
+    "revision": "f0b743d8013df3c01bcc",
+    "url": "/laxx/static/js/2.3923e217.chunk.js"
+  },
+  {
+    "revision": "72f12c6b13f14bd5f44c",
+    "url": "/laxx/static/js/main.9239eb06.chunk.js"
+  },
+  {
+    "revision": "8dafd914c2d9e2054db9",
+    "url": "/laxx/static/js/runtime-main.158bbd42.js"
+  }
+]);
